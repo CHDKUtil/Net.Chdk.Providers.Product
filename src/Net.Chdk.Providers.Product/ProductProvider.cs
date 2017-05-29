953c106147bfbe09e99b38f14d48566f815a2289
Dmitry Shechtman <damnian@gmail.com>
2017-05-29 20:14:10 +0300

    Add solution

---
+++ src/Net.Chdk.Providers.Product/ProductProvider.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Net.Chdk.Providers.Product
+{
+    sealed class ProductProvider : IProductProvider
+    {
+        #region Constants
+
+        private const string DataPath = "Data";
+        private string DataFileName = "products.json";
+
+        #endregion
+
+        #region Constructor
+
+        public ProductProvider()
+        {
+            data = new Lazy<string[]>(GetData);
+        }
+
+        #endregion
+
+        #region IProductProvider Members
+
+        public string[] GetProducts()
+        {
+            return Data;
+        }
+
+        #endregion
+
+        #region Serializer
+
+        private static readonly Lazy<JsonSerializer> serializer = new Lazy<JsonSerializer>(GetSerializer);
+
+        private static JsonSerializer Serializer => serializer.Value;
+
+        private static JsonSerializer GetSerializer()
+        {
+            return JsonSerializer.CreateDefault();
+        }
+
+        #endregion
+
+        #region Data
+
+        private readonly Lazy<string[]> data;
+
+        private string[] Data => data.Value;
+
+        private string[] GetData()
+        {
+            var filePath = Path.Combine(DataPath, DataFileName);
+            using (var reader = File.OpenText(filePath))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return Serializer.Deserialize<string[]>(jsonReader);
+            }
+        }
+
+        #endregion
+    }
+}
