9c43f604d3458ac0734a160df25921f1293f2c02
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 22:00:08 +0300

    Use DataProvider<T> Use Directories

--- src/Net.Chdk.Providers.Product/ProductProvider.cs
+++ src/Net.Chdk.Providers.Product/ProductProvider.cs
@@ -1,32 +1,21 @@
 ﻿using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
 using System.IO;
 
 namespace Net.Chdk.Providers.Product
 {
-    sealed class ProductProvider : IProductProvider
+    sealed class ProductProvider : DataProvider<string[]>, IProductProvider
     {
         #region Constants
 
-        private const string DataPath = "Data";
         private const string DataFileName = "products.json";
-
-        #endregion
-
-        #region Fields
-
-        private ILogger<ProductProvider> Logger { get; }
 
         #endregion
 
         #region Constructor
 
         public ProductProvider(ILoggerFactory loggerFactory)
+            : base(loggerFactory.CreateLogger<ProductProvider>())
         {
-            Logger = loggerFactory.CreateLogger<ProductProvider>();
-
-            data = new Lazy<string[]>(GetData);
         }
 
         #endregion
@@ -40,36 +29,16 @@ namespace Net.Chdk.Providers.Product
 
         #endregion
 
-        #region Serializer
-
-        private static readonly Lazy<JsonSerializer> serializer = new Lazy<JsonSerializer>(GetSerializer);
-
-        private static JsonSerializer Serializer => serializer.Value;
-
-        private static JsonSerializer GetSerializer()
-        {
-            return JsonSerializer.CreateDefault();
-        }
-
-        #endregion
-
         #region Data
 
-        private readonly Lazy<string[]> data;
-
-        private string[] Data => data.Value;
-
-        private string[] GetData()
+        protected override string GetFilePath()
         {
-            var filePath = Path.Combine(DataPath, DataFileName);
-            using (var reader = File.OpenText(filePath))
-            using (var jsonReader = new JsonTextReader(reader))
-            {
-                var data = Serializer.Deserialize<string[]>(jsonReader);
-                Logger.LogInformation("Products: {0}", JsonConvert.SerializeObject(data));
-                return data;
-            }
+            return Path.Combine(Directories.Data, DataFileName);
         }
+
+        protected override LogLevel LogLevel => LogLevel.Information;
+
+        protected override string Format => "Products: {0}";
 
         #endregion
     }
